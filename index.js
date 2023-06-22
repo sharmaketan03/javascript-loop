cd57a1c3c703d3ab838eb776c08c75777d70fa6e
ketansharma03 <ketan301024@gmail.com>
2023-06-22 12:08:20 +0530

    frst commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// const Btn=document.querySelector("button");
+// const Pera=document.querySelector("p");
+
+// Btn.onclick=()=>{
+//     for(let i=10; i>=1; i--){
+//         Pera.innerHTML+=i
+//     }
+// }
